3b285d44919c135ac3b39b35c45bd92b21d53097
Jessie Frazelle <jfrazelle@me.com>
2013-09-01 20:53:19 -0400

    update config.php

--- config.php
+++ config.php
@@ -1,16 +1,28 @@
 <?php
+date_default_timezone_set('America/New_York');
 
-$twitter['consumer_key'] = 'YOUR-CONSUMER-KEY';
-$twitter['consumer_secret'] = 'YOUR-CONSUMER-SECRET';
-$twitter['access_token'] = 'YOUR-ACCESS-TOKEN';
-$twitter['access_token_secret'] = 'YOUR-ACCESS-TOKEN-SECRET';
+$twitter = array(
+    'consumer_key' =>           'YOUR_CONSUMER_KEY',
+    'consumer_secret' =>        'YOUR_CONSUMER_SECRET',
+    'access_token' =>           'YOUR_ACCESS_TOKEN',
+    'access_token_secret' =>    'YOUR_TOKEN_SECRET'
+);
 
-$twitter['hashtag'] = 'outsidelands';
+$instagram = array(
+    'client_id' =>       'YOUR_CLIENT_ID',
+    'client_secret' =>  'YOUR_CLIENT_SECRET',
+    'website_url' =>    'http://YOUR_DOMAIN.co/hashtag-pull/instagram',
+    'redirect_uri' =>   'http://YOUR_DOMAIN.co/hashtag-pull/instagram',
+    'access_token' =>   'YOUR_ACCESS_TOKEN'
+);
 
-$db['host'] = '127.0.0.1';
-$db['user'] = 'root';
-$db['password'] = 'mypass';
-$db['name'] = 'twitter_vine';
+$db = array(
+    'host' =>       '127.0.0.1',
+    'user' =>       'root',
+    'password' =>   '',
+    'name' =>       'hashtag_pull'
+);
 
+$hashtag = 'legendsofsummer';
 
 ?>
